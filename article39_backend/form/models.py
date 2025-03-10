cd456fbc32549648646b698b579d65f15dbaafe7
tanimsk <sktanim5800@gmail.com>
2025-03-10 13:30:17 +0600

    api key

--- article39_backend/form/models.py
+++ article39_backend/form/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField, JSONField
+from django.contrib.postgres.fields import ArrayField
+from django.db.models import JSONField
 from uuid import uuid4
 
 class Artist(models.Model):
